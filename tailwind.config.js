f80aae3098b75748fef73abafc5512f59ef59e3c
jorgemonge <jorgemonge@uoc.edu>
2025-12-26 16:54:45 +0100

    vamos subiendo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.html", "./src/**/*.js", "./src/views/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          primary: "#000078",
+          master: "#73edff",
+          bg: "#d7fbff",
+        },
+      },
+      fontFamily: {
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ],
+        serif: ["Georgia", '"Times New Roman"', "serif"],
+      },
+    },
   },
   plugins: [],
 };
